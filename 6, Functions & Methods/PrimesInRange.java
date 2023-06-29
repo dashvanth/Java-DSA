306142eb5b79b24aaec17454b0f8ad256bd622d5
Dashvanth <dashvanthrajhc@gmail.com>
2023-06-29 22:04:14 +0530

    Learnings: Functions / Methods

---
+++ 6, Functions & Methods/PrimesInRange.java
@@ -0,0 +1,25 @@
+public class PrimesInRange {
+    public static boolean prime(int n) {
+        if (n == 2) {
+            return true;
+        }
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void primesInRange(int n) {
+        for (int i = 2; i <= n; i++) {
+            if (prime(i)) {
+                System.out.print(+i + " ");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        primesInRange(20);
+    }
+}
